066198194c74195be8a434cf533dba137441eb97
Pinaki <pinaki@pinakis.co.in>
2022-11-01 03:13:40 +0530

    -initial LZ module with AGW code

---
+++ 1/Terraform/LZ/hub.tf
@@ -0,0 +1,61 @@
+module "naming" {
+  source = "Azure/naming/azurerm"
+  suffix = ["pk-assemnt"]
+}
+
+locals {
+  location = "West Europe"
+}
+
+resource "azurerm_resource_group" "group" {
+  name     = module.naming.resource_group
+  location = local.location
+}
+
+# Network configuration ##
+#------------------------#
+resource "azurerm_virtual_network" "network" {
+  name                = "${module.naming.virtual_network}-spoke"
+  location            = local.location
+  resource_group_name = azurerm_resource_group.group.name
+  address_space       = ["10.10.0.0/16"]
+}
+
+# Subnet for AGW deployment #
+resource "azurerm_subnet" "gateway_subnet" {
+  name                 = module.naming.subnet
+  resource_group_name  = azurerm_virtual_network.network.resource_group_name
+  virtual_network_name = azurerm_virtual_network.network.name
+  address_prefixes     = ["10.10.1.0/24"]
+  service_endpoints    = ["Microsoft.KeyVault"]
+}
+
+# You must allow incoming Internet traffic on TCP ports 65200-65535 for the v2 SKU with the destination subnet as Any and source as GatewayManager service tag.
+# This port range is required for Azure infrastructure communication. These ports are protected (locked down) by Azure certificates.
+# External entities, including the customers of those gateways, can't communicate on these endpoints.
+resource "azurerm_network_security_group" "gateway_subnet_nsg" {
+  name                = module.naming.network_security_group
+  location            = local.location
+  resource_group_name = azurerm_resource_group.group.name
+
+  security_rule {
+    name                       = "agw-network-security-rule-healthcheck"
+    priority                   = 101
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "65200-65535"
+    source_address_prefix      = "GatewayManager"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_subnet_network_security_group_association" "gateway_subnet_nsg_association" {
+  subnet_id                 = azurerm_subnet.gateway_subnet.id
+  network_security_group_id = azurerm_network_security_group.gateway_subnet_nsg.id
+
+  depends_on = [azurerm_subnet.gateway_subnet]
+}
+#----------------------#
+# End of network block #
---
+++ 1/Terraform/LZ/main.tf
@@ -0,0 +1,38 @@
+## Main Application Gateway module ##
+#-----------------------------------#
+module "appgateway" {
+  source = "../modules/agw"
+
+  resource_group_name = azurerm_resource_group.group.name
+  location            = local.location
+  agw_subnet_id       = azurerm_subnet.gateway_subnet.id
+
+
+  backend_address_pools = [
+    {
+      "name" = "pool-dev"
+      //"ip_addresses" = [module.certified_vm.private_ip]
+    }
+  ]
+
+  request_routing_rules = [
+    {
+      "backend_address_pool_name"  = "pool-dev"
+      "backend_http_settings_name" = "dev-http-settings"
+      "name"                       = "dev-routing"
+    }
+  ]
+
+  backend_http_settings = [
+    {
+      "name"            = "dev-http-settings"
+      "port"            = "80"
+      "protocol"        = "Http"
+      "request_timeout" = 100
+    }
+  ]
+
+
+}
+#--------------------#
+# End of AGW module #
